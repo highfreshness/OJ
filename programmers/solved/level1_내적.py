f91002df0b330de0b8f2e4862372b3b5f0bca045
highfreshness <meteo0718@gmail.com>
2022-01-28 21:18:28 +0900

    modify

--- programmers/solved/level1_내적.py
+++
@@ -1,5 +0,0 @@
-def solution(a, b):
-    answer = 0
-    for x in range(len(a)):
-        answer += a[x] * b[x]
-    return answer
--- programmers/solved/level1_음양 더하기.py
+++
@@ -1,3 +0,0 @@
-def solution(absolutes, signs):
-    answer = sum([(-x if y == False else x) for x, y in zip(absolutes, signs)])
-    return answer
