34369c06ac06e1f43383ff121ecf75ccb56508ce
mintchoco777 <meteo0718@gmail.com>
2022-02-05 23:25:21 +0900

    OJ

--- programmers/solved/level2_가장 큰 수.py
+++
@@ -1,7 +0,0 @@
-def solution(num):
-    num = list(map(str, num))
-    num.sort(key=lambda x: x * 3, reverse=True)
-    return str(int("".join(num))) 
-
-numbers = [3, 30, 34, 5, 9]
-print(solution(numbers))
