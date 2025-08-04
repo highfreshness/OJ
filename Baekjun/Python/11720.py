5ebdad83da1be85186fd5ab75da919e625e3058a
highfreshness <meteo0718@gmail.com>
2025-08-04 22:01:05 +0900

    250804 update

--- Baekjun/Python/11720.py
+++ Baekjun/Python/11720.py
@@ -1,10 +1,18 @@
-# 제한 시간 1초
-# 시간 복잡도 : O(N) - 100
-n = int(input())
-m = input()
-result = 0
+"""
+아이디어:
+    1. 첫번째 줄의 입력이 필요 없는 것으로 예상
+    2. 리스트를 이용해 공백 없는 문자열 저장
+    3. 각 문자열을 정수로 변환 후 합산하고 출력
+시간복잡도: O(N)
+변수:
+    N : 문자열의 길이
+    numbers : 공백 없는 문자열을 저장 할 리스트
+"""
 
-for x in m:
-    result += int(x) 
-    
-print(result)
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+numbers = list(input().strip())
+print(sum(map(int, numbers)))
